9c176b35a8181afff384c305b9db1047f6f25060
boommanpro <boommanpro@gmail.com>
2025-03-25 22:12:04 +0800

    feat(样式调整和编辑框代码重构):

--- src/form-components/form-outputs/styles.tsx
+++ src/form-components/form-outputs/styles.tsx
@@ -7,8 +7,7 @@ export const FormOutputsContainer = styled.div`
   border-top: 1px solid var(--semi-color-border);
   width: 100%;
   padding: 10px 10px 10px;
-  background: #f5f5fc;
-
+  background: linear-gradient(#f5f5fc 0%, rgba(0, 0, 0, 0.02) 100%);
   :global(.semi-tag .semi-tag-content) {
     font-size: 10px;
   }
