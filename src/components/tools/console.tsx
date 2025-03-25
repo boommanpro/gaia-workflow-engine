b77358bd7d7baaf098b0071daf42f94152939c0b
boommanpro <boommanpro@gmail.com>
2025-03-25 22:21:34 +0800

    feat(console代码编写):

--- src/components/tools/console.tsx
+++ src/components/tools/console.tsx
@@ -1,26 +1,39 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { useClientContext, usePlayground } from '@flowgram.ai/free-layout-editor';
 import { Button, Tooltip } from '@douyinfe/semi-ui';
 import { IconTerminal } from '@douyinfe/semi-icons';
 
+import { CodeEditorModal } from '../code-editor-modal';
+
 export const Console = () => {
   const ctx = useClientContext();
-
+  const [showData, setShowData] = useState('');
   const playground = usePlayground();
+  const [showModal, setShowModal] = useState(false);
   const consoleJSON = useCallback(async () => {
-    console.log(ctx.document.toJSON());
+    const jsonData = JSON.stringify(ctx.document.toJSON(), null, 2);
+    setShowData(jsonData); // 先更新 showData
+    setShowModal(true); // 然后再显示 Modal
   }, [ctx]);
 
   return (
-    <Tooltip content={'Terminal Show JSON Data'}>
-      <Button
-        disabled={playground.config.readonly}
-        type="tertiary"
-        icon={<IconTerminal />}
-        theme="borderless"
-        onClick={consoleJSON}
+    <>
+      <Tooltip content={'Terminal Show JSON Data'}>
+        <Button
+          disabled={playground.config.readonly}
+          type="tertiary"
+          icon={<IconTerminal />}
+          theme="borderless"
+          onClick={consoleJSON}
+        />
+      </Tooltip>
+      <CodeEditorModal
+        value={showData}
+        language={'json'}
+        visible={showModal}
+        onVisibleChange={setShowModal}
       />
-    </Tooltip>
+    </>
   );
 };
