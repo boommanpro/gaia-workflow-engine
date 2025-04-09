b7486bfb2600f38ca9b856d1f6349968fef8479a
boommanpro <boommanpro@gmail.com>
2025-04-09 20:51:15 +0800

    feat(升级依赖):

---
+++ rsbuild.config.ts
@@ -0,0 +1,21 @@
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginLess } from '@rsbuild/plugin-less';
+import { defineConfig } from '@rsbuild/core';
+
+export default defineConfig({
+  plugins: [pluginReact(), pluginLess()],
+  source: {
+    entry: {
+      index: './src/main.tsx',
+    },
+    /**
+     * support inversify @injectable() and @inject decorators
+     */
+    decorators: {
+      version: 'legacy',
+    },
+  },
+  html: {
+    title: 'demo-free-layout',
+  },
+});
