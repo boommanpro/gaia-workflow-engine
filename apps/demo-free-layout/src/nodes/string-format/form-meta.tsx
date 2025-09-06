477074bd4fd1ac91685c0aa740ecda2aff06b4ab
boommanpro <boommanpro@gmail.com>
2025-09-06 12:07:27 +0800

    feat(仿照http输出优化):

--- apps/demo-free-layout/src/nodes/string-format/form-meta.tsx
+++ apps/demo-free-layout/src/nodes/string-format/form-meta.tsx
@@ -3,30 +3,31 @@
  * SPDX-License-Identifier: MIT
  */
 
-import { FormMeta, FormRenderProps } from '@flowgram.ai/free-layout-editor';
-import { createInferInputsPlugin } from '@flowgram.ai/form-materials';
+import {FormMeta, FormRenderProps} from '@flowgram.ai/free-layout-editor';
+import {createInferInputsPlugin, DisplayOutputs} from '@flowgram.ai/form-materials';
 
-import { FormHeader, FormContent } from '../../form-components';
-import { CodeNodeJSON } from './types';
-import { Outputs } from './components/outputs';
-import { Inputs } from './components/inputs';
-import { Code } from './components/code';
-import { defaultFormMeta } from '../default-form-meta';
+import {FormContent, FormHeader} from '../../form-components';
+import {CodeNodeJSON} from './types';
+import {Inputs} from './components/inputs';
+import {Code} from './components/code';
+import {defaultFormMeta} from '../default-form-meta';
+import {Divider} from "@douyinfe/semi-ui";
 
-export const FormRender = ({ form }: FormRenderProps<CodeNodeJSON>) => (
-  <>
-    <FormHeader />
-    <FormContent>
-      <Inputs />
-      <Code />
-      <Outputs />
-    </FormContent>
-  </>
+export const FormRender = ({form}: FormRenderProps<CodeNodeJSON>) => (
+    <>
+        <FormHeader/>
+        <FormContent>
+            <Inputs/>
+            <Code/>
+            <Divider />
+            <DisplayOutputs displayFromScope/>
+        </FormContent>
+    </>
 );
 
 export const formMeta: FormMeta = {
-  render: (props) => <FormRender {...props} />,
-  effect: defaultFormMeta.effect,
-  validate: defaultFormMeta.validate,
-  plugins: [createInferInputsPlugin({ sourceKey: 'inputsValues', targetKey: 'inputs' })],
+    render: (props) => <FormRender {...props} />,
+    effect: defaultFormMeta.effect,
+    validate: defaultFormMeta.validate,
+    plugins: [createInferInputsPlugin({sourceKey: 'inputsValues', targetKey: 'inputs'})],
 };
